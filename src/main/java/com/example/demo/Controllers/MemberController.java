8b07863d7c0d0d074e9133326fa542128f1dd24d
Simanden <Silasbm1996@hotmail.com>
2018-05-17 11:06:57 +0200

    receipt ikke fungerende

--- src/main/java/com/example/demo/Controllers/MemberController.java
+++ src/main/java/com/example/demo/Controllers/MemberController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -37,6 +38,14 @@ public class MemberController {
 
         return "index";
     }
+
+    @GetMapping("/memberReceipt")
+    public String memberReceipt(@RequestParam("id") int id, Model model){
+        model.addAttribute("member", memberRepo.read(id));
+        return "memberReceipt";
+    }
+
+
 
     private boolean sessionController(HttpSession session){
         if(session.getAttribute("status") != null && session.getAttribute("status").equals("1")){
